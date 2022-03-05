50c409cfd4e1aa1485e5b2903aa17d82a13e485e
ajay gadhwal <ajaygadhwal1@gmail.com>
2022-03-05 19:56:15 +0530

    Fixed bottom navigator issue

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -43,6 +43,29 @@ export class AdminComponent implements OnInit {
     // if (localStorage.getItem('sm|sidebar-toggle') === 'true') {
     //   document.body.classList.toggle('sm-sidenav-toggled');
     // }
+    // var menu_bar = document.querySelector(".sc-bottom-bar") as any;
+    // var menu_item = document.querySelectorAll(".sc-menu-item") as any;
+    // var menu_indicator = document.querySelector(".sc-nav-indicator") as any;
+    // var menu_current_item = document.querySelector(".sc-current") as any;
+    // var menu_position;
+
+    // if (menu_current_item && menu_indicator) {
+    //   menu_position = menu_current_item.offsetLeft - 16;
+    //   menu_indicator.style.left = menu_position + "px";
+    //   menu_bar.style.backgroundPosition = menu_position - 8 + "px";
+    //   menu_item.forEach((select_menu_item: any) => {
+    //     select_menu_item.addEventListener("click",  (e: any) => {
+    //       e.preventDefault();
+    //       menu_position = e.offsetLeft - 16;
+    //       menu_indicator.style.left = menu_position + "px";
+    //       menu_bar.style.backgroundPosition = menu_position - 8 + "px";
+    //       [...select_menu_item.parentElement.children].forEach((sibling) => {
+    //         sibling.classList.remove("sc-current");
+    //       });
+    //       select_menu_item.classList.add("sc-current");
+    //     });
+    //   });
+    // }
   }
 
   toggleSidebar(event: MouseEvent): void {
